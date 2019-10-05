9089863a7dad9f8317531ceeac5335a14c21036d
Iregon <0alsidavi0>
2019-10-05 17:43:25 +0200

    Create Logger

---
+++ simple-logger/src/main/java/LoggerHandler.java
@@ -0,0 +1,44 @@
+import com.alessandro.simplelogger.interfaces.ILogger;
+
+import java.util.ArrayList;
+
+public class LoggerHandler {
+
+    private ArrayList<ILogger> loggers = new ArrayList<ILogger>();
+    private static LoggerHandler handler = new LoggerHandler();
+
+    public static LoggerHandler getInstance() {
+        return handler;
+    }
+
+    public LoggerHandler() {
+    }
+
+    /**
+     * Add a logger to logger's list
+     * @param logger New logger
+     */
+    public void addLogger(ILogger logger) {
+        loggers.add(logger);
+    }
+
+    /**
+     * Call info method of all loggers
+     * @param s Message to send to all loggers
+     */
+    public void info(String s) {
+        loggers.forEach(logger -> logger.info(s));
+    }
+
+    /**
+     * Get the numbers of loggers
+     * @return numbers of loggers
+     */
+    public int getLoggersNum() {
+        return loggers.size();
+    }
+
+    public void clearAllLoggers() {
+        loggers.clear();
+    }
+}
---
+++ simple-logger/src/main/java/interfaces/ILogger.java
@@ -0,0 +1,5 @@
+package interfaces;
+
+public interface ILogger {
+    void info(String s);
+}
